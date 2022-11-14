afac6677ae55271478a88da5f881e1cb7e4050b4
RodolfoJaques <rodolfojaques13@gmail.com>
2022-11-14 17:55:22 -0300

    feat: criação do projeto, estruturação de pastas e pagina home

---
+++ src/pages/home/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const HomeStyle = styled.main`
+
+    height: 100vh;
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
